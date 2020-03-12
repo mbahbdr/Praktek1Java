36de9a35aaa5f49799e3f6a1be4742da025e95c2
mbahbdr <44817829+mbahbdr@users.noreply.github.com>
2020-03-12 15:10:47 +0800

    Add files via upload

---
+++ helllo.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class {
+	public static void main (String[]args)
+	{
+		System.out.println ("Moh yoga badriawan s");
+		System.out.println ("90231004");
+		System.out.println ("D3 TekhnikInformatika");
+	}
+}
